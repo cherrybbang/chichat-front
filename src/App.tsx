093eba29fdaed9d9c6a3a18714a182485c7d06cc
JiYu <eunjun1018@gmail.com>
2026-03-19 19:37:37 +0900

    헤더 작업

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css'
 import MainPage from './components/MainPage'
+import Header from './components/Header'
 
 function App() {
 
   return (
     <>
+      <Header />
       <section id="center">
         <MainPage />
       </section>
---
+++ src/components/Header.tsx
@@ -0,0 +1,19 @@
+function Header() {
+  return (
+    <header className="header">
+      <div className="header-inner">
+        <div className="logo">
+          ChitChat
+        </div>
+        <nav>
+          <ul>
+            <li>채팅기록</li>
+            <li>마이페이지</li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  )
+}
+
+export default Header
