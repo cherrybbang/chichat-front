5376841a4d5bf21c5b050528c75a78ff37234dc2
JiYu <eunjun1018@gmail.com>
2026-03-19 19:15:23 +0900

    메인페이지 작업

---
+++ src/components/MainPage.tsx
@@ -0,0 +1,10 @@
+function MainPage() {
+  return (
+    <div>
+      <h1>Ask Me Anything...</h1>
+      <button className="main-page-btn">click!</button>
+    </div>
+  )
+}
+
+export default MainPage
